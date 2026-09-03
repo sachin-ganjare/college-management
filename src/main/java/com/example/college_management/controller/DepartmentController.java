73f1da0e35c4d8a4d0d24a70033a4e5d2ae5220e
Sachin Ganjare <sganjare29@gmail.com>
2026-09-03 09:20:12 +0530

    create controller - department

---
+++ src/main/java/com/example/college_management/controller/DepartmentController.java
@@ -0,0 +1,38 @@
+package com.example.college_management.controller;
+
+import com.example.college_management.entity.Department;
+import com.example.college_management.service.DepartmentService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/departments")
+public class DepartmentController {
+    private final DepartmentService departmentService;
+
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping("/{id}")
+    public Department getDepartmentById(@PathVariable Long id) {
+        return departmentService.getDepartmentById(id).orElseThrow(()->new RuntimeException("Department Not Found!"));
+    }
+
+    @PostMapping
+    public Department createDepartment(@RequestBody Department department){
+        return departmentService.createDepartment(department);
+    }
+
+    @PutMapping("/{id}")
+    public Department updateDepartment(@PathVariable Long id, @RequestBody Department department) {
+        return departmentService.updateDepartment(id, department);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteDepartment(@PathVariable Long id) {
+        departmentService.deleteDepartment(id);
+        return "Department deleted successfully!";
+    }
+
+
+}
