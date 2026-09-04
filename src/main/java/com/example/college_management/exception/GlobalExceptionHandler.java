765b872ba59ca8f87f837c8b9a08edc20c0011cd
Sachin Ganjare <sganjare29@gmail.com>
2026-09-04 19:40:45 +0530

    exception handler

---
+++ src/main/java/com/example/college_management/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.college_management.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, Object> handleResourceNotFound(ResourceNotFoundException exception) {
+        return Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", 404,
+                "error", "Not Found",
+                "message", exception.getMessage()
+        );
+    }
+}
